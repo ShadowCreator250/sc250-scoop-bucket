003ad6129da8632991679fe3abbe2f6d5ee79ff5
ShadowCreator250 <shadowcreator250+github@gmail.com>
2022-05-02 14:41:40 +0200

    replace placeholder w actual values in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = " ShadowCreator250/sc250-scoop-bucket:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
